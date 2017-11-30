6043d897381149ffd334b3bac6098f26a0810715
William Leingang <wleingang15@mail.wou.edu>
2017-11-29 16:46:33 -0800

    You can view the stuff in the DB now, so that's good I guess...

--- HW8-1/App_Data/up.sql
+++ HW8-1/App_Data/up.sql
@@ -28,3 +28,37 @@ CREATE TABLE Classifications(
 	FOREIGN KEY (idg) REFERENCES Genres(id),
 	PRIMARY KEY (id)
 )
+
+INSERT INTO dbo.Artists(aName, DOB, city) VALUES
+	('M.C. Escher', '06/17/1898', 'Leeuwarden, Netherlands'),
+	('Leonardo Da Vinci', '05/02/1519', 'Vinci, Italy'),
+	('Hatip Mehmed Efendi', '11/18/1680', 'Unknown'),
+	('Salvador Dali', '05/11/1904', 'Figueres, Spain')
+GO
+
+INSERT INTO dbo.Artworks(aName, idA) VALUES
+	('Circle Limit III', 1),
+	('Twon Tree', 1),
+	('Mona Lisa', 2),
+	('The Vitruvian Man', 2),
+	('Ebru', 3),
+	('Honey Is Sweeter Than Blood', 4)
+GO
+
+INSERT INTO dbo.Genres(gName) VALUES
+	('Tesselation'),
+	('Surrealism'),
+	('Portrait'),
+	('Renaissance')
+GO
+
+INSERT INTO dbo.Classifications(idA, idG) VALUES
+	(1, 1),
+	(2, 1),
+	(2, 2),
+	(3, 3),
+	(3, 4),
+	(4, 4),
+	(5, 1),
+	(6, 2)
+GO
--- HW8-1/Controllers/HomeController.cs
+++ HW8-1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HW8_1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,7 @@ namespace HW8_1.Controllers
 {
     public class HomeController : Controller
     {
+        private dbContext db = new dbContext();
         // GET: Home
         public ActionResult Index()
         {
@@ -17,19 +19,35 @@ namespace HW8_1.Controllers
         // GET: Art
         public ActionResult Artists()
         {
-            return View();
+            return View(db.Artists.ToList());
         }
 
         // GET: Artists
         public ActionResult Artworks()
         {
-            return View();
+            var tmp = db.Artworks;
+            List<ArtistArtworkVM> arts = new List<ArtistArtworkVM>();
+
+            foreach (var i in tmp)
+            {
+                arts.Add(new ArtistArtworkVM(db.Artists.Where(s => s.id == (i.idA)).FirstOrDefault(), i));
+            }
+
+            return View(arts);
         }
 
         // GET: Classifications
         public ActionResult Classifications()
         {
-            return View();
+            var tmp = db.Artworks;
+            List<GenreArtworkVM> classies = new List<GenreArtworkVM>();
+
+            foreach (var i in tmp)
+            {
+                classies.Add(new GenreArtworkVM(db.Genres.Where(s => s.id == (db.Classifications.Where(d => d.idA == i.id)).Select(f => f.idG).FirstOrDefault()).FirstOrDefault(), i));
+            }
+
+            return View(classies);
         }
     }
 }
